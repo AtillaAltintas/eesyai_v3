66ede02b7cc476643e921d5fa22e6a9348120fb2
atilla <galoisati@gmail.com>
2025-07-24 23:15:35 +0200

    adress changed to eesyai

--- backend/main.py
+++ backend/main.py
@@ -76,7 +76,9 @@ async def get_db():
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://eesyai.com",
+      "https://www.eesyai.com",
+      "https://eesyai-v3.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
