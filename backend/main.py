ded93cc5fa5e04e496137dc3975bf47a76b8df17
atilla <galoisati@gmail.com>
2025-07-25 20:54:55 +0200

    main CORS changed

--- backend/main.py
+++ backend/main.py
@@ -76,7 +76,12 @@ async def get_db():
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+      "http://localhost:3000",
+      "https://eesyai-v3.onrender.com",
+      "https://eesyai.vercel.app",
+      "https://www.eesyai.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
