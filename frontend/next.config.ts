37993858d35fbb4c2ba0edeaaba4137f97f210d5
atilla <galoisati@gmail.com>
2025-07-25 23:39:16 +0200

    page next 5

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,22 +1,7 @@
-//next.config.ts
-import { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      // proxy all /api/auth/* 
-      {
-        source: '/api/auth/:path*',
-        destination: ${process.env.NEXT_PUBLIC_API_URL}/auth/:path*,
-      },
-      // proxy all /api/ai/*  
-      {
-        source: '/api/ai/:path*',
-        destination: ${process.env.NEXT_PUBLIC_API_URL}/api/ai/:path*,
-      },
-    ]
-  },
-}
+  /* config options here */
+};
 
-export default nextConfig
-
+export default nextConfig;
