0172dffe6b73071fc74d15fe5c40f00e6f8c2c0c
atilla <galoisati@gmail.com>
2025-08-02 16:05:14 +0200

    next config 2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,5 @@
 // next.config.ts
-import { NextConfig } from 'next'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         source: '/api/auth/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_URL}/auth/:path*`,
       },
-      // proxy all /api/ai/* → https://eesyai-v3.onrender.com/api/ai/*
+      // proxy all /api/ai/*   → https://eesyai-v3.onrender.com/api/ai/*
       {
         source: '/api/ai/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_URL}/api/ai/:path*`,
@@ -19,3 +19,4 @@ const nextConfig: NextConfig = {
 }
 
 export default nextConfig
+
